f5dd51b2b3a1961283384ce478a11d9b5c305681
MarioDeNisi <mariodenisi@gmail.com>
2025-09-09 22:18:29 +0200

    generic keepAlive.ts

--- api/keepAlive.ts
+++ api/keepAlive.ts
@@ -1,22 +1,33 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-async function keepAliveAttempt(): Promise<{status: number, text: string}> {
-  const response = await fetch('https://smartalk.onrender.com/keep_alive');
+async function keepAliveAttempt(object: string): Promise<{ status: number; text: string }> {
+  let baseUrl = "";
+  if (object === "1") baseUrl = "https://smartalk.onrender.com";
+  if (object === "2") baseUrl = "https://constructo-website.onrender.com";
+
+  const response = await fetch(`${baseUrl}/keep_alive`);
   const text = await response.text();
+
   if (response.status === 200) {
     return { status: 200, text };
   } else {
     // ritardo random tra 0 e 5 secondi
     const delay = Math.random() * 5000;
     await new Promise(res => setTimeout(res, delay));
-    const retry = await fetch('https://smartalk.onrender.com');
+    const retry = await fetch(baseUrl);
     return { status: retry.status, text: "Home reloaded" };
   }
 }
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const { status, text } = await keepAliveAttempt();
+    const object = req.query.object as string | undefined;
+
+    if (object !== "1" && object !== "2") {
+      return res.status(400).json({ error: "Error on 'object' query parameter" });
+    }
+
+    const { status, text } = await keepAliveAttempt(object);
     res.status(status).json({ response: text });
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
